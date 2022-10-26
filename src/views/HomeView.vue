ac80ce2e5c82e4cd3689d835a31035be886c069c
HMWGithub <H.MaxWilson@hotmail.com>
2022-10-26 13:43:54 +0100

    updated styling and changed anchor tags for new Link component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,31 @@
 <script setup>
+import Link from "/src/components/layouts/Link.vue";
 
 </script>
 
 <template>
   <main>
     <div class="page">
-      <h1 class="page__title">Harrison Max-Wilson</h1>
-      <p>
-        I write code professionally, and often just write for fun. See my
-        complete employment and academic history on
-        <a href="https://www.linkedin.com/in/HMaxWilson" class="link"
-          >LinkedIn</a
-        >.
-      </p>
-      <p>
-        I previously wrote programs for academic assignments, however now write
-        them for personal and work purposes. You can find all of my most recent
-        personal projects on
-        <a href="https://github.com/HMaxWilson" class="link">GitHub</a>.
-      </p>
-      <p>
-        For work enquiries, you can message me via
-        <a href="https://www.linkedin.com/in/HMaxWilson" class="link"
-          >LinkedIn</a
-        >
-        or <a href="mailto:Web.HMW@hotmail.com" class="link">Email</a>.
-      </p>
+      <h1 class="page__title my-4">Harrison Max-Wilson</h1>
+      <div class="space-y-8">
+        <p>
+          I write code professionally, and often just write for fun. See my
+          complete employment and academic history on
+          <Link href="https://www.linkedin.com/in/HMaxWilson" target="_blank" class="link">LinkedIn</Link>.
+        </p>
+        <p>
+          I previously wrote programs for academic assignments, however now write
+          them for personal and work purposes. You can find all of my most recent
+          personal projects on
+          <Link href="https://github.com/HMaxWilson" target="_blank" class="link">GitHub</Link>.
+        </p>
+        <p>
+          For work enquiries, you can message me via
+          <Link href="https://www.linkedin.com/in/HMaxWilson" target="_blank" class="link">LinkedIn</Link>
+          or
+          <Link href="mailto:Web.HMW@hotmail.com" class="link">Email</Link>.
+        </p>
+      </div>
     </div>
   </main>
 </template>
